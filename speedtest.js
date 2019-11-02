537e43a4017513f73949d42091c1c0c14a0af057
IsaacXu <xuhonggang@hotmail.com>
2019-11-03 00:11:58 +0800

    测速网去广告 策略更新

--- speedtest.js
+++ speedtest.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         测速网去广告
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      1.0.3
 // @description  谁用谁知道
 // @author       Meter
 // @match        *://www.speedtest.cn/*
-// @updateURL    https://tampermonkey.isaacxu.com/speedtest.js
+// @updateURL    https://app.isaacxu.com/tampermonkey/speedtest.js
 // @require      http://libs.baidu.com/jquery/2.0.0/jquery.min.js
 // @supportURL   https://github.com/meterXu/mtMonkeyJs
 // @grant        none
@@ -13,7 +13,7 @@
 
 (function() {
     'use strict';
-    $(".col-2,.tmall,.qrCode,.advertising-upCaption").remove();
+    $(".col-2,.tmall,.qrCode,.advertising-upCaption,.advertisingSpace,.speed-top-ads").remove();
     $("img[src='../../images/adv/index-top.png']").parent().parent().remove();
     $("img[src='/images/qq.png?da63ad6d80a46582316364f64a4627c4']").parent().parent().remove();
     $(".col-8").css({
